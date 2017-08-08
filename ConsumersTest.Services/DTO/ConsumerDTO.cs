62da77b392936c611a575fe8caf157fc3e00df66
EugenKuleshov <EugenKuleshov@KULESHOVEE10>
2017-08-08 12:19:00 +0300

    ConsumerTest.Services: add consumer automapper profile

--- ConsumersTest.Services/DTO/ConsumerDTO.cs
+++ ConsumersTest.Services/DTO/ConsumerDTO.cs
@@ -10,6 +10,8 @@ namespace ConsumersTest.Services.DTO
 
         public string LastName { get; set; }
 
+        public string FullName { get; set; }
+
         public string Email { get; set; }
 
         public DateTime DateOfBirth { get; set; }
---
+++ ConsumersTest.Services/_automapper/Profiles/ConsumerProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ConsumersTest.Services.DTO;
+using ConsumersTest.Wcf.Contracts.Data;
+
+namespace ConsumersTest.Services._automapper.Profiles
+{
+    internal class ConsumerProfile: Profile
+    {
+        public ConsumerProfile()
+        {
+            CreateMap<Consumer, ConsumerDTO>()
+                .ForMember(dest => dest.FullName, opt => opt.ResolveUsing(src => $"{src.FirstName} {src.LastName}"));
+
+            CreateMap<ConsumerDTO, Consumer>();
+        }
+    }
+}
